dda98a8a329f323e4ad865f697b4e5c1128d9eeb
Michael Sproul <micsproul@gmail.com>
2015-03-19 16:37:34 -0700

    Auto-upload docs to Github pages.

---
+++ upload-docs.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+rev=$(git rev-parse --short HEAD)
+
+cd target/docs
+
+# Add a page to redirect to the main crate.
+echo '<meta http-equiv="refresh" content="0; url=rust_sequence_trie/index.html">' > index.html
+
+git init
+git config user.name "Michael Sproul"
+git config user.email "micsproul@gmail.com"
+
+git remote add upstream "https://$GH_TOKEN@github.com/michaelsproul/rust_sequence_trie.git"
+git fetch upstream && git reset upstream/gh-pages
+
+git add -A .
+git commit -m "Rebuild pages at ${rev}"
+git push -q upstream HEAD:gh-pages
